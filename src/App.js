c35239e86bf8387e4925c2fc8d133766553345e3
Zulfiqar Ali <01zulfiqarali@gmail.com>
2021-12-04 15:39:01 +0500

    Add Navbar to App & update routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
+          <Route path="*" element={<Navbar />} />
+        </Routes>
+        <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/home" element={<Home />} />
+          <Route path="home" element={<Home />} />
+          <Route path="shop" element={<div>Shop</div>} />
+          <Route path="shop/cart" element={"Cart"} />
           <Route path="*" element={<h1>Nothing here</h1>} />
         </Routes>
       </BrowserRouter>
