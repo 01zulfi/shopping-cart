1d1e8b910c78648d4bd3023c678f4c4c08d8fd5c
Zulfiqar Ali <01zulfiqarali@gmail.com>
2021-12-05 13:49:46 +0500

    Add uniqueId() & tests for asteroids.js

--- src/asteroids.js
+++ src/asteroids.js
@@ -1,31 +1,41 @@
+const uniqueId = () =>
+  Math.floor(Math.random() * Date.now() + Math.random() * 999999);
+
 const asteroids = [
   {
     name: "1 Ceres",
     price: "253",
+    id: uniqueId(),
   },
   {
     name: "4 Vesta",
     price: "299",
+    id: uniqueId(),
   },
   {
     name: "25143 Itokawa",
     price: "99",
+    id: uniqueId(),
   },
   {
     name: "243 Ida",
     price: "147",
+    id: uniqueId(),
   },
   {
     name: "433 Eros",
     price: "444",
+    id: uniqueId(),
   },
   {
     name: "253 Mathilde",
     price: "121",
+    id: uniqueId(),
   },
   {
     name: "101955 Bennu",
     price: "191",
+    id: uniqueId(),
   },
 ];
 
