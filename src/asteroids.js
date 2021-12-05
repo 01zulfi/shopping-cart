997c6c8e87d7624b0899bf37fbf56e0a542f8e59
Zulfiqar Ali <01zulfiqarali@gmail.com>
2021-12-05 12:56:42 +0500

    Add asteroids.js

---
+++ src/asteroids.js
@@ -0,0 +1,32 @@
+const asteroids = [
+  {
+    name: "1 Ceres",
+    price: "253",
+  },
+  {
+    name: "4 Vesta",
+    price: "299",
+  },
+  {
+    name: "25143 Itokawa",
+    price: "99",
+  },
+  {
+    name: "243 Ida",
+    price: "147",
+  },
+  {
+    name: "433 Eros",
+    price: "444",
+  },
+  {
+    name: "253 Mathilde",
+    price: "121",
+  },
+  {
+    name: "101955 Bennu",
+    price: "191",
+  },
+];
+
+export default asteroids;
