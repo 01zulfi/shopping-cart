6df3aa3374174302c97f60a019e1db974339fdc8
Zulfiqar Ali <01zulfiqarali@gmail.com>
2021-12-05 17:47:14 +0500

    Add functionality to navigate asteroid routes in Shop.js

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,9 +1,24 @@
 import React from "react";
 import asteroids from "../asteroids";
+import { useNavigate } from "react-router";
 
 const Shop = () => {
+  const navigate = useNavigate();
+
+  const navigateRoute = (event) => {
+    const id = Number(event.currentTarget.getAttribute("data-id"));
+    const targetAsteroid = asteroids.find((asteroid) => asteroid.id === id);
+
+    navigate(`/shop/asteroid/${targetAsteroid.urlName}`);
+  };
+
   const item = (asteroid) => (
-    <div key={asteroid.id} className="item-div" data-id={asteroid.id}>
+    <div
+      onClick={navigateRoute}
+      key={asteroid.id}
+      className="item-div"
+      data-id={asteroid.id}
+    >
       <hr />
       <p className="asteroid-name">Name : {asteroid.name}</p>
       <p className="asteroid-price">Price: {asteroid.price}</p>
--- src/tests/Shop.test.js
+++ src/tests/Shop.test.js
@@ -1,24 +1,53 @@
 import { render } from "@testing-library/react";
 import "@testing-library/jest-dom";
+import userEvent from "@testing-library/user-event";
+import { BrowserRouter, Routes, Route, useParams } from "react-router-dom";
 import Shop from "../components/Shop";
 
+let container = {};
+
+const Test = () => {
+  let params = useParams();
+  return <h1>{params.name}</h1>;
+};
+
+beforeEach(() => {
+  ({ container } = render(
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Shop />} />
+        <Route path="shop/asteroid/:name" element={<Test />} />
+      </Routes>
+    </BrowserRouter>
+  ));
+});
+
 test("shop renders", () => {
-  const { container } = render(<Shop />);
   expect(container.firstChild).toMatchSnapshot();
   expect(container.firstChild).toHaveClass("shop");
 });
 
 test("items render", () => {
-  const { container } = render(<Shop />);
-  const item = container.querySelectorAll(".item-div");
-  expect(item).toHaveLength(7);
+  const items = container.querySelectorAll(".item-div");
+  expect(items).toHaveLength(7);
 });
 
 test("first asteroid renders correct info", () => {
-  const { container } = render(<Shop />);
   const firstName = container.querySelector(".asteroid-name");
   const firstPrice = container.querySelector(".asteroid-price");
 
   expect(firstName).toHaveTextContent("1 Ceres");
   expect(firstPrice).toHaveTextContent("253");
+});
+
+test("clicking navigates to correct page", () => {
+  const item = container.querySelector(".item-div");
+
+  userEvent.click(item);
+
+  const name = document.querySelector("h1");
+  expect(name).toHaveTextContent("1-ceres");
+
+  const shop = document.querySelector(".shop");
+  expect(shop).toBeNull();
 });
