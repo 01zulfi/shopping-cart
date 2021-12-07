762cba8ad04277c422c7f3afaa985fe2a4e59199
Zulfiqar Ali <01zulfiqarali@gmail.com>
2021-12-07 18:42:24 +0500

    Refactor CartPage component

--- src/components/CartPage.js
+++ src/components/CartPage.js
@@ -1,12 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+import CartItem from "./CartItem";
 import cart from "../CartManager";
 
 const CartPage = () => {
-  const cartItem = ({ name, price, id }) => {
+  const [items, setItems] = useState(cart.cart);
+  const [total, setTotal] = useState(cart.total);
+
+  const setTotalHandler = (value) => setTotal(value);
+
+  const deleteButtonHandler = (event) => {
+    const id = Number(event.target.getAttribute("data-id"));
+    cart.remove(id);
+    setTotalHandler(cart.total);
+    setItems(cart.cart);
+  };
+
+  const mapCallback = (item) => {
     return (
-      <div className="cart-item-div" key={id}>
-        <h2>{name}</h2>
-        <p>{price}</p>
+      <div className="cart-item-wrapper" key={item.id}>
+        <CartItem
+          name={item.name}
+          price={item.price}
+          id={item.id}
+          quantity={item.quantity}
+          setTotalHandler={setTotalHandler}
+        />
+        <button
+          data-id={item.id}
+          className="delete-item"
+          onClick={deleteButtonHandler}
+        >
+          Delete
+        </button>
+        <hr />
       </div>
     );
   };
@@ -14,8 +40,8 @@ const CartPage = () => {
   return (
     <div className="cart">
       <h1>Cart</h1>
-      {cart.cart.map((item) => cartItem(item))}
-      <h3>{cart.total}</h3>
+      {items.map(mapCallback)}
+      <h3>{total}</h3>
     </div>
   );
 };
