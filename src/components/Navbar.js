354bb5ea3b9b4b47d57722d8de924819c90a7914
Zulfiqar Ali <01zulfiqarali@gmail.com>
2021-12-10 17:48:59 +0500

    Add cart icon

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import cartIcon from "../icons/cart.svg";
 import "../styles/Navbar.css";
 
 const Navbar = ({ cartLength }) => {
   return (
     <div className="navbar">
-      <h1>Asteroids</h1>
+      <h1>Asteroid Store</h1>
       <div className="nav-links">
         <ul>
           <li>
@@ -14,8 +15,11 @@ const Navbar = ({ cartLength }) => {
           <li>
             <Link to="/shop">Shop</Link>
           </li>
-          <li>
-            <Link to="/shop/cart">Cart {cartLength}</Link>
+          <li className="cart-link">
+            <Link to="/shop/cart">
+              <img src={cartIcon} alt="cart" />
+              <p>{cartLength}</p>
+            </Link>
           </li>
         </ul>
       </div>
