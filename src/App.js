00e9c94c0f26966c435886acbc61c0dc46b6bec8
Zulfiqar Ali <01zulfiqarali@gmail.com>
2021-12-10 20:05:57 +0500

    Add basename to BrowserRouter

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
 
   return (
     <div className="app">
-      <BrowserRouter>
+      <BrowserRouter basename="/shopping-cart">
         <Routes>
           <Route path="*" element={<Navbar cartLength={cartLength} />} />
         </Routes>
