68674e6c18a1b5b09c8f72e7118c87de0259cc70
01zulfi <85733202+01zulfi@users.noreply.github.com>
2022-05-07 13:15:09 +0500

    Updates

--- src/tests/ItemPage.test.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
-import "@testing-library/jest-dom";
-import ItemPage from "../components/ItemPage";
-
-test("does not render for undefined route name", () => {
-  const { container } = render(<ItemPage />);
-  expect(container.firstChild).toBeNull();
-});
